f9a7b74cadb44e2616004bd9736cbe3b90872aa7
26harvintilavat <tilavatharvin@gmail.com>
2026-03-25 22:18:33 +0530

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Authentication System API",
+    description="A FastAPI project for user registration, login, JWT authentication, protected routes, and role-based access control.",
+    version="0.1.0"
+)
+
+@app.get("/", tags=["Root"])
+def root() -> dict[str, str]:
+    return {"message": "Authentication System API is running"}
